38872a9120f75763c15b24ed3e57959169c11611
Aamir <djzaamir@gmail.com>
2024-01-17 17:10:36 +0000

    Reading text via pdf

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
 st.set_page_config(page_title='PDF-GPT', layout = 'wide', initial_sidebar_state = 'auto')
 with st.sidebar:
@@ -16,4 +17,21 @@ with st.sidebar:
     )
 
     st.markdown("## Inspired by the Youtube tutorial of [Prompt Engineer](https://www.youtube.com/watch?v=RIWbalZ7sTo&ab_channel=PromptEngineering)")
-    
+
+
+def main():
+
+    st.header("Chat with the PDF")
+    pdf = st.file_uploader("Upload PDF", type="pdf")
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        st.write(text)
+
+if __name__ == "__main__":
+    main()

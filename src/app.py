43d1e0123a6ae9a882f581a94c24df4de7526eaa
Aamir <djzaamir@gmail.com>
2024-01-18 18:40:36 +0000

    Computing embeddings only if they dont already exists

--- src/app.py
+++ src/app.py
@@ -31,38 +31,47 @@ def main():
 
     st.header("Chat with the PDF")
     pdf = st.file_uploader("Upload PDF", type="pdf")
-  
+    
+
 
     if pdf is not None:
         filename = str(pdf.name).split('.')[0]
-        st.write(filename)
-
-        pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000, # Tokens
-            chunk_overlap=200, # Overlap between the adjacent chunks
-                               # Both adjacent chunks will share 200 tokens 
-                               # This helps in not loosing the context of the 
-                               # conversation 
-            length_function=len
-        )
-
-        chunks = text_splitter.split_text(text)
-
-        #Embeddings 
-        # TODO,this will hit OpenAI API with each RUN
-        embeddings = OpenAIEmbeddings()
-        vector_store = faiss.FAISS.from_texts(chunks, embeddings)
-
-        # Will create a new directory for each embedding
         embeddings_file = os.path.join("openai_computed_embeddings", f"{filename}")
 
-        vector_store.save_local(embeddings_file)
+        embedding_already_computed = os.path.exists(embeddings_file)
+
+        if not embedding_already_computed:
+
+            st.write(f"Computing Embeddings For -> {filename}")
+
+
+            pdf_reader = PdfReader(pdf)
+            
+            
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000, # Tokens
+                chunk_overlap=200, # Overlap between the adjacent chunks
+                                # Both adjacent chunks will share 200 tokens 
+                                # This helps in not loosing the context of the 
+                                # conversation 
+                length_function=len
+            )
+
+            chunks = text_splitter.split_text(text)
+
+            #Embeddings 
+            # TODO,this will hit OpenAI API with each RUN
+            embeddings = OpenAIEmbeddings()
+            vector_store = faiss.FAISS.from_texts(chunks, embeddings)
+
+            # Will create a new directory for each embedding
+            embeddings_file = os.path.join("openai_computed_embeddings", f"{filename}")
+
+            vector_store.save_local(embeddings_file)
 
 
 if __name__ == "__main__":
